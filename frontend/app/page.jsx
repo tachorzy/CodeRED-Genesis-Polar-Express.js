d6ece670a1898d9e9484aa93b8d9958732dd92d5
ChristopherOosthuizen <christopher.jan.oosthuizen@gmail.com>
2024-02-11 09:33:47 -0600

    Revert "added back"
    This reverts commit d9030bab937fd2b445fd80471c625274efd12d74.

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,16 +1,15 @@
 import NavBar from "@/components/Navbar";
 import ChatBox from "@/components/chat/ChatBox";
 import React from "react";
-import useSpotify from "/hooks/useSpotify";
 import Infographics from "@/components/Infographics";
-
 export default function Home() {
-  const {getAmadeus,getGenres,getLocationBasedOnGenre, getCat,runPrompt} = useAI()
-  console.log(runPrompt("get me a flight from Mexico to the US"))
+  const {getToken} = useSpotify();
+  const token = getToken();
+  console.log(token)
   return (
     <main>
       <div className="bg-violet-500 grid grid-cols-2 h-screen pl-24">  
-          <div className={"flex flex-col"}>
+        <div className={"flex flex-col"}>
           <NavBar/>
           <Infographics/>
         </div>
