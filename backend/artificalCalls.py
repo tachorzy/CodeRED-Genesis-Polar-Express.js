20fb03f0256761eac353fbc49847d4008aad0e5a
ChristopherOosthuizen <christopher.jan.oosthuizen@gmail.com>
2024-02-11 10:27:21 -0600

    there

--- backend/artificalCalls.py
+++ backend/artificalCalls.py
@@ -1,6 +1,8 @@
 import os 
-os.environ["OPENAI_API_KEY"] = "apikey"
+os.environ["OPENAI_API_KEY"] = "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"
+import logging
 
+logger = logging.getLogger(__name__)
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 from llama_index import ( SimpleDirectoryReader, ServiceContext, KnowledgeGraphIndex,)
 from llama_index.graph_stores import SimpleGraphStore
@@ -8,7 +10,9 @@ from llama_index.llms import OpenAI
 from IPython.display import Markdown, display 
 from llama_index.storage.storage_context import StorageContext
 import datetime
-
+import requests
+import os
+import json
 llm = OpenAI(temperature=0, model="gpt-4")
 llm_low_temp = OpenAI(temperature=0.8, model="gpt-4")
 service_context = ServiceContext.from_defaults(llm=llm, chunk_size=512)
@@ -26,22 +30,35 @@ Layoverstuff = SimpleDirectoryReader("musicGenres").load_data()
 layIndex = VectorStoreIndex.from_documents(Layoverstuff, service_context=service_context_low_temp)
 layoverllm= layIndex.as_chat_engine()
 plainTextToAPIPrompt = """You are Amadeus api prompt creator. Using your knowledge of the Amadeus api convert a plain text request into a Get request to the api.
-Keep in mind that the api url is https://test.api.amadeus.com/ and there are some manditory paramaters. if a date is not specified use the  {date}. If number of adults is not speicifed assume 1 adult. If no location is specified assume houston.
+Keep in mind that the api url is https://test.api.amadeus.com/ and there are some manditory paramaters. if a date is not specified use the  {date}. You must specifiy the 3 digit airport codes in captial letters not the place name, in the query.  If number of adults is not speicifed assume 1 adult. If no location is specified assume houston. Cap output at 5
 
 <plainTextRequest>{request}</plainTextRequest>
 
 
 After examine the request:
-- breifly explain your answer in less than 300 words, include the specific paramaters you set and why and the specific api version and endpoint you used.
+- breifly explain your answer in less than 100 words, include the specific paramaters you set and why and the specific api version and endpoint you used.
 - return your output in format:
 Answer: |<request>|"""
 
 
 def convertPlainToAPI(plaintext):
+    logger.info("started on llm call")
     response = str(flightAPIllm.query(plainTextToAPIPrompt.format(request=plaintext, date=datetime.datetime.now())))
-    print(response)
     url = response[response.find("|")+1: response.rfind("|")]
-    return url
+    os.system("rm tmp")
+    os.system('''curl "https://test.api.amadeus.com/v1/security/oauth2/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=ryA0cYZi5nArKfr0mBGjfU9PzwCy5hYE&client_secret=9ug5uSsAQWaVhTT9" > tmp
+''')
+
+    result = open('tmp', 'r').read()
+    token = json.loads(result)['access_token']
+    os.system('''curl "{main}" \
+     -H "Authorization: Bearer {token}" > tmp
+'''.format(main=url, token=token))
+    result = open('tmp', 'r').read()
+    print(result)
+    return result
 
 
 musicalGenres = """You are a genre picker. Given a query for a flight and destination. Pick 5 musical genres that fit the vibe of the country of destination. 
@@ -79,6 +96,7 @@ Location:
 """
 def getLocationBasedOnGenre(genres):
     response = str(musical_llm.query(genreLocation.format(genres=genres)))
+    print(response)
     return response.split("Location:\n")[1].split("\n")
 
 categories="""You are a categrorizer, of tasks, given a prompt categroize the prompt into 3 categories documented below:
@@ -89,13 +107,14 @@ categories="""You are a categrorizer, of tasks, given a prompt categroize the pr
 
 <task>{task}</task>
 
-After anylsizing the genres:
+After anylsizing the task:
 - justify your answer in less than 100 words
 - finally output your answer in the following format: 
 Category: <categoryNum>
 """
 def getCategory(task):
     response = str(musical_llm.query(categories.format(task=task)))
+    print(response)
     return int(response.split("Category: ")[1].split(" ")[0])
 
 slayover = """You are a layover activity reccommender given a airport and a amount of time. Return activities to do in the airport or very close by if the time is long enough. Be very specific about the specific location do not give overly generic answers that can apply to basically any airport.
