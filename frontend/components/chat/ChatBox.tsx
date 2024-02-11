0a2943b98f63272da56b9cbd71d966fe37be63ee
tachorzy <tachorzyad@gmail.com>
2024-02-11 03:14:26 -0600

    Added scrollbar to chatbox

--- frontend/components/chat/ChatBox.tsx
+++ frontend/components/chat/ChatBox.tsx
@@ -39,11 +39,20 @@ const ChatBox = () => {
             <form className="flex flex-col px-5">
                 {/*Honeypot field */}
                 <input name="_gotcha" type="hidden" className="hidden"/>
-                <div className="flex flex-col h-[34rem] py-5 gap-y-6">
+                <div className="flex flex-col-reverse h-[34rem] pb-6 py-5 gap-y-6 overflow-y-scroll">
                     <TextBubble sender="AI" message={"test tes test  test"}/>
                     <TextBubble sender="user" message={"BLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH BLAH"}/>
+                    <TextBubble sender="AI" message={"test tes test  test"}/>
+                    <TextBubble sender="AI" message={"test tes test  test"}/>
+                    <TextBubble sender="AI" message={"test tes test  test"}/>
+                    <TextBubble sender="AI" message={"test tes test  test"}/>
+                    <TextBubble sender="AI" message={"test tes test  test"}/>
+                    <TextBubble sender="AI" message={"test tes test  test"}/>
+                    <TextBubble sender="AI" message={"test tes test  test"}/>
+                    <TextBubble sender="AI" message={"test tes test  test"}/>
+
                 </div>
-                <div className="grid grid-cols-2 gap-x-5 py-5">
+                <div className="grid grid-cols-2 gap-x-5 py-5 z-50">
                     <div className="bg-violet-900 rounded-l-2xl h-14 w-[17%] flex flex-row">
                         <Image src="/icons/flight.svg" width={47} height={47} className="justify-center z-40 bg-violet-900 rounded-l-2xl" alt=""></Image>
                         <input name="_gotcha" type="" className="text-base text-violet-700 flex flex-col bg-violet-100 opacity-[85%] rounded-2xl h-14 w-[90%] z-30 absolute px-2 py-0 pl-14"/>
--- frontend/components/chat/TextBubble.tsx
+++ frontend/components/chat/TextBubble.tsx
@@ -18,13 +18,13 @@ const TextBubble = (props: {sender: string, message: string}) => {
     //         </div>
     //     )
     // }
-    let textQualities = "mr-auto bg-violet-200 text-violet-700"
+    let textQualities = "mr-auto bg-violet-200 text-violet-700 "
     
     if (props.sender === "user") {
-        textQualities = "ml-auto bg-violet-400 text-violet-50"
+        textQualities = "ml-auto bg-violet-400 text-violet-50 "
     }
     return( 
-        <div className={`flex flex-col ${textQualities} min-h-16 w-1/2 rounded-2xl p-3`}>
+        <div className={`flex flex-col ${textQualities} w-1/2 rounded-2xl p-3`}>
             <h1 className="text-xs font-semibold leading-relaxed">{props.message}</h1>
         </div>
 
